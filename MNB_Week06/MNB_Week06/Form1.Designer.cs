782cbfe4011a87da9d7652eb9641ae294d4fe4a7
Magyar Melinda <magyarm@outlook.com>
2021-10-29 06:41:57 +0200

    .

--- MNB_Week06/MNB_Week06/Form1.Designer.cs
+++ MNB_Week06/MNB_Week06/Form1.Designer.cs
@@ -29,11 +29,14 @@ namespace MNB_Week06
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.chartRateData = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chartRateData)).BeginInit();
             this.SuspendLayout();
@@ -41,32 +44,62 @@ namespace MNB_Week06
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Location = new System.Drawing.Point(0, 40);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(374, 344);
             this.dataGridView1.TabIndex = 0;
             // 
             // chartRateData
             // 
-            chartArea1.Name = "ChartArea1";
-            this.chartRateData.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chartRateData.Legends.Add(legend1);
-            this.chartRateData.Location = new System.Drawing.Point(380, 0);
+            chartArea3.Name = "ChartArea1";
+            this.chartRateData.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chartRateData.Legends.Add(legend3);
+            this.chartRateData.Location = new System.Drawing.Point(380, 40);
             this.chartRateData.Name = "chartRateData";
-            series1.ChartArea = "ChartArea1";
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.chartRateData.Series.Add(series1);
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chartRateData.Series.Add(series3);
             this.chartRateData.Size = new System.Drawing.Size(582, 344);
             this.chartRateData.TabIndex = 1;
             this.chartRateData.Text = "chart1";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 12);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(106, 20);
+            this.dateTimePicker1.TabIndex = 2;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.valueChanged);
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker2.Location = new System.Drawing.Point(124, 14);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(99, 20);
+            this.dateTimePicker2.TabIndex = 3;
+            this.dateTimePicker2.ValueChanged += new System.EventHandler(this.valueChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(253, 14);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedValueChanged += new System.EventHandler(this.valueChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(985, 450);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.chartRateData);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
@@ -81,6 +114,9 @@ namespace MNB_Week06
 
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.DataVisualization.Charting.Chart chartRateData;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
--- MNB_Week06/MNB_Week06/Form1.cs
+++ MNB_Week06/MNB_Week06/Form1.cs
@@ -17,12 +17,22 @@ namespace MNB_Week06
     public partial class Form1 : Form
     {
         BindingList<RateData> Rates = new BindingList<RateData>();
+            MNBArfolyamServiceSoapClient mnbService = new MNBArfolyamServiceSoapClient();
 
         public Form1()
         {
             InitializeComponent();
-            
+
             dataGridView1.DataSource = Rates;
+
+            setChart();
+
+            //LoadXML();
+            LoadCurrencies();
+        }
+
+        private void setChart()
+        {
             chartRateData.DataSource = Rates;
             chartRateData.Series[0].ChartType = SeriesChartType.Line;
             chartRateData.Series[0].XValueMember = "Date";
@@ -31,35 +41,61 @@ namespace MNB_Week06
             chartRateData.ChartAreas[0].AxisX.MajorGrid.Enabled = false;
             chartRateData.ChartAreas[0].AxisY.MajorGrid.Enabled = false;
             chartRateData.ChartAreas[0].AxisY.IsStartedFromZero = false;
-
-            LoadXML();
         }
 
         private void LoadXML()
         {
             XmlDocument xml = new XmlDocument();
             xml.LoadXml(CallWebService());
+            Rates.Clear();
             foreach (XmlElement item in xml.DocumentElement)
             {
                 RateData r = new RateData();
-                r.Currency = ((XmlElement)item.ChildNodes[0]).GetAttribute("curr");
-                r.Date = DateTime.Parse(item.GetAttribute("date"));
-                r.Value = decimal.Parse(((XmlElement)item.ChildNodes[0]).InnerText);
-                int unit = int.Parse(((XmlElement)item.ChildNodes[0]).GetAttribute("unit"));
-                if (unit != 0) r.Value = r.Value / unit;
-                Rates.Add(r);
+                
+                if (item.HasChildNodes)
+                {
+                    r.Currency = ((XmlElement)item.ChildNodes[0]).GetAttribute("curr");
+                    r.Date = DateTime.Parse(item.GetAttribute("date"));
+                    r.Value = decimal.Parse(((XmlElement)item.ChildNodes[0]).InnerText);
+                    int unit = int.Parse(((XmlElement)item.ChildNodes[0]).GetAttribute("unit"));
+                    if (unit != 0) r.Value = r.Value / unit;
+                    Rates.Add(r);
+                }
             }
+            setChart();
         }
+
+        void LoadCurrencies()
+        {
+            MNBArfolyamServiceSoapClient mnbService = new MNBArfolyamServiceSoapClient();
+            GetCurrenciesRequestBody req = new GetCurrenciesRequestBody();
+            var resp = mnbService.GetCurrencies(req);
+            BindingList<string> currencies = new BindingList<string>();
+            XmlDocument xml = new XmlDocument();
+            xml.LoadXml(resp.GetCurrenciesResult);
+            foreach (XmlElement item in xml.DocumentElement.FirstChild.ChildNodes)
+            {
+                currencies.Add(item.InnerText);
+            }
+
+
+                comboBox1.DataSource = currencies;
+        }
+
 
         string CallWebService()
         {
-            MNBArfolyamServiceSoapClient mnbService = new MNBArfolyamServiceSoapClient();
             GetExchangeRatesRequestBody req = new GetExchangeRatesRequestBody();
-            req.currencyNames = "EUR";
-            req.startDate = "2021-01-01";
-            req.startDate = "2021-06-30";
+            req.currencyNames = comboBox1.Text;
+            req.startDate = dateTimePicker1.Value.ToString("yyyy-MM-dd");
+            req.endDate = dateTimePicker2.Value.ToString("yyyy-MM-dd");
             var resp = mnbService.GetExchangeRates(req);
             return resp.GetExchangeRatesResult;
+        }
+
+        private void valueChanged(object sender, EventArgs e)
+        {
+            LoadXML();
         }
     }
 }
